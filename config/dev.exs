fdc5a4a4388e5b8243f8be9eef69454d4394d01e
Joakim Nylen <me@jnylen.nu>
2020-03-01 17:12:09 +0100

    Move config from an subapp to main config, fix footer

--- config/dev.exs
+++ config/dev.exs
@@ -9,6 +9,20 @@ config :database, Database.Repo,
   password: "metagraph",
   database: "metagraph"
 
+config :authorization, Authorization.Guardian,
+  # optional
+  allowed_algos: ["HS512"],
+  # optional
+  verify_module: Guardian.JWT,
+  issuer: "Metagraph",
+  ttl: {30, :days},
+  allowed_drift: 2000,
+  # optional
+  verify_issuer: true,
+  # generated using: JOSE.JWK.generate_key({:oct, 16}) |> JOSE.JWK.to_map |> elem(1)
+  secret_key: %{"k" => "4Cg2QCFvSq5Xuh47mYCiEw", "kty" => "oct"},
+  serializer: Authorization.Guardian
+
 # For development, we disable any cache and enable
 # debugging and code reloading.
 #
