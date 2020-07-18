283421f29f5e78a61cee0c7f15c467c23bc3952a
Joakim Nylén <git@joakim.nylen.nu>
2020-07-18 11:50:57 +0200

    Add settings page

--- apps/frontend/lib/frontend_web/controllers/page_controller.ex
+++ apps/frontend/lib/frontend_web/controllers/page_controller.ex
@@ -2,8 +2,28 @@ defmodule FrontendWeb.PageController do
   use FrontendWeb, :controller
 
   def index(conn, _params) do
-
-
     render(conn, "index.html")
+  end
+
+  def profile(conn, _params) do
+    conn
+    |> render("profile.html", changeset: Database.Account.update_changeset(conn.assigns.current_user))
+  end
+
+  def update(conn, %{"account" => params}) do
+    Database.Account.update_changeset(conn.assigns.current_user, params)
+    |> Database.Repo.update()
+    |> case do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Saved!")
+        |> render("profile.html", changeset: Database.Account.update_changeset(user))
+      {:error, changeset} ->
+        changeset |> IO.inspect()
+
+        conn
+        |> put_flash(:error, "Unable to save!")
+        |> render("profile.html", changeset: changeset)
+    end
   end
 end
