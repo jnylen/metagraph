ad0c9a48846a72e481ef323821fc4754b0eb3b3a
Joakim Nylén <git@joakim.nylen.nu>
2020-07-26 22:38:47 +0200

    Fix skogsra

--- apps/database/mix.exs
+++ apps/database/mix.exs
@@ -9,7 +9,11 @@ defmodule Database.MixProject do
     {:jason, "~> 1.0"},
 
     # Pagination
-    {:scrivener_ecto, "~> 2.0"}
+    {:scrivener_ecto, "~> 2.0"},
+
+    # Config
+    {:skogsra, "~> 2.3"},
+    {:yamerl, "~> 0.7"}
   ]
   @aliases [
     "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
