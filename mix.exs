cbc65768296e2304c9707ee3877548c00178cf53
Joakim Nylén <git@joakim.nylen.nu>
2020-07-26 21:25:33 +0200

    Launch

--- mix.exs
+++ mix.exs
@@ -1,23 +1,42 @@
 defmodule Umbrella.MixProject do
   use Mix.Project
 
+  @deps [
+    {:pkg_deb, "~> 0.3"},
+    {:cowlib, "~> 2.9.1", override: true}
+  ]
+
   def project do
     [
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: @deps,
+      version: File.read!("VERSION") |> String.trim(),
+      releases: [
+        vind: [
+          include_executables_for: [:unix],
+          applications: [
+            runtime_tools: :permanent,
+            api: :permanent,
+            frontend: :permanent,
+            database: :permanent,
+            graph: :permanent
+          ],
+          steps: [:assemble, &PkgDeb.create(&1, deb_config())]
+        ]
+      ]
     ]
   end
 
-  # Dependencies listed here are available only for this
-  # project and cannot be accessed from applications inside
-  # the apps folder.
-  #
-  # Run "mix help deps" for examples and options.
-  defp deps do
+  defp deb_config() do
     [
-      {:cowlib, "~> 2.9.1", override: true}
-
+      vendor: "Joakim Nylén",
+      maintainers: ["Joakim Nylén <joakim@nylen.nu>"],
+      homepage: "https://metagraph.wiki",
+      base_path: "/opt",
+      external_dependencies: [],
+      owner: [user: "ubuntu", group: "ftpgroup"],
+      description: "Metagraph Application"
     ]
   end
 end
