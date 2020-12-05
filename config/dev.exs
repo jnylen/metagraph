33cedcad2917debf4ea47f08e882a48345df76ba
Joakim Nylén <git@joakim.nylen.nu>
2020-12-05 09:54:36 +0100

    Fix webpack issue

--- config/dev.exs
+++ config/dev.exs
@@ -73,7 +73,8 @@ config :frontend, FrontendWeb.Endpoint,
       "node_modules/webpack/bin/webpack.js",
       "--mode",
       "development",
-      "--watch-stdin",
+      "--watch",
+      "--watch-options-stdin",
       cd: Path.expand("../apps/frontend/assets", __DIR__)
     ]
   ]
