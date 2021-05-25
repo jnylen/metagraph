fc0ea4c11fb3bf37d0f9eb1733b2febfa662c6d4
Joakim Nylén <git@joakim.nylen.nu>
2021-05-25 22:52:29 +0200

    Add Graph repo config

--- config/prod.exs
+++ config/prod.exs
@@ -23,6 +23,7 @@ config :database, Database.Repo,
   pool_size: String.to_integer(System.get_env("POOL_SIZE", "100"))
 
 config :frontend, Frontend.Endpoint,
+  live_view: [signing_salt: System.get_env("LIVE_VIEW_SALT")],
   url: [host: System.get_env("APP_HOST", "localhost")],
   http: [
     ip: {0, 0, 0, 0},
@@ -33,3 +34,21 @@ config :frontend, Frontend.Endpoint,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: secret_key_base,
   check_origin: false
+
+config :meilisearch,
+  endpoint: System.get_env("MEILI_URL"),
+  api_key: System.get_env("MEILI_API_KEY")
+
+config :authorization, Authorization.Guardian,
+  allowed_algos: ["HS512"],
+  verify_module: Guardian.JWT,
+  issuer: "Metagraph",
+  ttl: {30, :days},
+  allowed_drift: 2000,
+  verify_issuer: true,
+  secret_key: %{"k" => System.get_env("GUARDIAN_SECRET_KEY"), "kty" => "oct"},
+  serializer: Authorization.Guardian
+
+config :dlex, Graph.Repo,
+  hostname: System.get_env("GRAPH_HOST"),
+  port: String.to_integer(System.get_env("GRAPH_PORT", "9080"))
